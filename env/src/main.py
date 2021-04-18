2fa5f4a54bd1ab97f07a46e089da2bbbb2e481d2
Gabriel Gitonga <gabriel.gitonga@bskglobaltech.com>
2021-04-18 12:58:21 +0300

    Adding global http configurations

--- env/src/main.py
+++ env/src/main.py
@@ -2,8 +2,41 @@ import os
 
 from flask import Flask, jsonify
 from api.config.config import DevelopmentConfig, ProductionConfig, TestingConfig
+from api.utils.responses import response_with
+import api.utils.responses as resp
+
+
 app = Flask(__name__)
 
+if os.environ.get('WORK_ENV') == 'PROD':
+    app_config = ProductionConfig
+elif os.environ.get('WORK_ENV') == 'TEST':
+    app_config = TestingConfig
+else:
+    app_config = DevelopmentConfig
+    
+    
+# START GLOBAL HTTP CONFIGURATIONS
+@app.after_request
+def add_header(response):
+    return response
+
+@app.errorhandler(400)
+def bad_request(e):
+    logging.error(e)
+    return response_with(resp.BAD_REQUEST_400)
+
+@app.errorhandler(500)
+def server_error(e):
+    logging.error(e)
+    return response_with(resp.SERVER_ERROR_500)
+
+@app.errorhandler(404)
+def not_found(e):
+    logging.error(e)
+    return response_with(resp.SERVER_ERROR_404)
+
+# END GLOBAL HTTP CONFIGURATIONS
 
 def create_app(config):
     app = Flask(__name__)
@@ -12,13 +45,6 @@ def create_app(config):
     with app.app_context():
         db.create_all()
     return app
-
-if os.environ.get('WORK_ENV') == 'PROD':
-    app_config = ProductionConfig
-elif os.environ.get('WORK_ENV') == 'TEST':
-    app_config = TestingConfig
-else:
-    app_config = DevelopmentConfig
 
 app.config.from_object(app_config)
 
